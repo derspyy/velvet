50ce6ddab9ae34101f7e4466a69ffa19a78788dd
derspyy <65043391+derspyy@users.noreply.github.com>
2022-12-02 08:58:35 -0300

    Aim for stable quilt releases

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-use serde_json::Value;
+use serde::Deserialize;
 use colored::{Colorize, control};
 
 mod install_velvet;
@@ -8,17 +8,28 @@ mod get_minecraft_dir;
 mod get_mods;
 pub mod write_json;
 
+#[derive(Deserialize)]
+struct Versions {
+    version: String
+}
+
 fn main() {
 
     #[cfg(target_os = "windows")]
     control::set_virtual_terminal(true).unwrap();
 
     println!("Welcome to {}!", "Velvet".purple().bold());
-    let version_response: Value = reqwest::blocking::get("https://meta.quiltmc.org/v3/versions/loader")
+    let version_response: Vec<Versions> = reqwest::blocking::get("https://meta.quiltmc.org/v3/versions/loader")
         .expect("Failed to fetch latest Quilt version.")
         .json()
         .unwrap();
-    let quilt_version = version_response[0]["version"].as_str().unwrap();
+    let mut quilt_version = String::new();
+    for x in version_response {
+        if x.version.contains("-") == false {
+            quilt_version = x.version;
+            break
+        }
+    }
     println!("Latest Velvet Quilt version: {}.", &quilt_version.purple().italic());
     println!("Enter minecraft version:");
     let mut mc_version = String::new();
@@ -27,7 +38,7 @@ fn main() {
         .expect("Couldn't read.");
     mc_version = rm_newline(mc_version);
     println!("Selected version: {}", &mc_version.purple().italic());
-    let path_mods = install_velvet::run(&mc_version, &quilt_version.to_string());
+    let path_mods = install_velvet::run(&mc_version, &quilt_version);
     get_mods::run(&mc_version, path_mods);
     println!("Done. Enjoy! {}", "Don't forget to restart the minecraft launcher.".red().underline());
 
