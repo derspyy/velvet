c759be67c4db5ad0ced0186e04e9f1a8c44dc23f
derspyy <65043391+derspyy@users.noreply.github.com>
2022-12-04 00:00:42 -0300

    Refactor Json parsing from generic to typed

--- src/write_json.rs
+++ src/write_json.rs
@@ -1,78 +1,75 @@
 use std::fs::File;
 use reqwest::blocking::Client;
-use serde_json::json;
 use chrono::Utc;
 use colored::Colorize;
+use serde::{Serialize, Deserialize};
+use std::collections::HashMap;
+
 const VANILLA_ARGS: &str = "-Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M";
+
+#[derive(Serialize, Deserialize)]
+struct LauncherProfiles {
+    profiles: HashMap<String, Profiles>
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct Profiles {
+    created: String,
+    icon: String,
+    java_args: String,
+    java_dir: String,
+    last_used: String,
+    last_version_id: String,
+    name: String,
+    _type: String
+}
+
 pub fn write_version(mc: &String, velvet: &String, z: &File) {
 
     let url = format!("https://meta.quiltmc.org/v3/versions/loader/{}/{}/profile/json", &mc, &velvet);
     let client = Client::new();
     let json: serde_json::Value = client
-            .get(&url)
-            .header("User-Agent", "Velvet")
-            .send()
-            .expect("Couldn't communicate with Quilt's meta server.")
-            .json()
-            .unwrap();
-
-    /*
-    Quilt Installer Hack
-    json["libraries"][11] = json!({
-    "name": (format!("net.fabricmc:intermediary:{}", &mc)),
-    "url": "https://maven.fabricmc.net/"
-    });
-    */
-
+    .get(&url)
+    .header("User-Agent", "Velvet")
+    .send()
+    .expect("Couldn't communicate with Quilt's meta server.")
+    .json()
+    .unwrap();
     serde_json::to_writer_pretty(z, &json).unwrap();
 }
 
 pub fn write_profile(mc: &String, velvet: &String, x: &File) -> String {
-    let mut json: serde_json::Value = serde_json::from_reader(x).unwrap();
+    let mut json: LauncherProfiles = serde_json::from_reader(x).unwrap();
 
     // This copies the "Latest Release" java arguments and version.
-    let release_id = get_release(&json);
-    let args = match get_args(&json, &release_id) {
-        json!(null) => json!(VANILLA_ARGS),
-        x => x.to_owned()
-    };
-    let java_dir = match get_version(&json, &release_id) {
-        json!(null) => "",
-        x => x.as_str().unwrap()
-    };
-    let time = Utc::now();
+    let mut args = VANILLA_ARGS;
+    let mut dir = "";
 
-    json["profiles"][format!("velvet-quilt-loader-{}", &mc)] = json!({
-        "created": &time,
-        "icon": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAAGFBMVEUAAAB7GWmyF1rqR1a3msT/nJfp0+r///+virVnAAAAAXRSTlMAQObYZgAAAI9JREFUKM990cENA0EIQ9FpgRbcglugBVr4Lbj9HCaRNtnZcHwSYMFaa63Vvb7rB5JS8QeiAqCfIA0C6yO/0CFJDQkcgWqSsTIlTpCSSJJoCAdAHqtqY/oOncgz492pIyTInrEK7kDtrci21AeQx+9olxwXWP0e2GRf4AaLRPYOV32CbSVb1TzAtsDloR94AcQTfFFwBa/NAAAAAElFTkSuQmCC",
-        "javaArgs": format!("-Dloader.modsDir=.velvet/mods/{0} -Dloader.configDir=.velvet/config/{0} {1}", &mc, &args.as_str().unwrap()),
-        "javaDir": &java_dir,
-        "lastUsed": &time,
-        "lastVersionId": format!("quilt-loader-{}-{}", &velvet, &mc),
-        "name": format!("Velvet {}", &mc),
-        "type": "custom",
-
-    });
-    serde_json::to_string_pretty(&json).unwrap()
-}
-fn get_release(x: &serde_json::Value) -> Option<String> {
-    for (name, value) in x["profiles"].as_object().unwrap() {
-        if value["type"] == json!("latest-release") {
-            println!("Copying Java directory from: {}.", "latest-release".purple().italic());
-            return Some(name.to_owned())
+    for x in json.profiles.values() {
+        if x._type == "latest-release" {
+            println!("Copying Java directory and arguments from: {} profile.", "Latest Release".purple().italic());
+            args = &x.java_args;
+            dir = &x.java_dir;
+            break
         }
     }
-    None
+
+    let time = Utc::now();
+
+    let new_profile = Profiles {
+    created: time.to_string(),
+    icon: String::from("data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAAGFBMVEUAAAB7GWmyF1rqR1a3msT/nJfp0+r///+virVnAAAAAXRSTlMAQObYZgAAAI9JREFUKM990cENA0EIQ9FpgRbcglugBVr4Lbj9HCaRNtnZcHwSYMFaa63Vvb7rB5JS8QeiAqCfIA0C6yO/0CFJDQkcgWqSsTIlTpCSSJJoCAdAHqtqY/oOncgz492pIyTInrEK7kDtrci21AeQx+9olxwXWP0e2GRf4AaLRPYOV32CbSVb1TzAtsDloR94AcQTfFFwBa/NAAAAAElFTkSuQmCC"),
+    java_args: format!("-Dloader.modsDir=.velvet/mods/{0} -Dloader.configDir=.velvet/config/{0} {1}", mc, args),
+    java_dir: dir.to_owned(),
+    last_used: time.to_string(),
+    last_version_id: format!("quilt-loader-{}-{}", &velvet, &mc),
+    name: format!("Velvet {}", &mc),
+    _type: "custom".to_owned()
+    };
+
+    json.profiles.insert(format!("velvet-quilt-loader-{}", &mc), new_profile);
+
+    serde_json::to_string_pretty(&json).unwrap()
 }
-fn get_version<'a>(x: &'a serde_json::Value,y: &'a Option<String>) -> &'a serde_json::Value {
-    match y {
-        Some(z) => &x["profiles"][&z]["javaDir"],
-        None => &json!(null)
-    }
-}
-fn get_args<'a>(x: &'a serde_json::Value,y: &'a Option<String>) -> &'a serde_json::Value {
-    match y {
-        Some(z) => &x["profiles"][&z]["javaArgs"],
-        None => &json!(null)
-    }
-}
+
