e0f270e3b757c0da4604a4f81c7b0efc8771dccd
derspyy <65043391+derspyy@users.noreply.github.com>
2022-12-15 23:48:27 -0300

    Refactor JSON writer

--- src/write_json.rs
+++ src/write_json.rs
@@ -7,6 +7,7 @@ use serde_json::{Value, json};
 use std::collections::HashMap;
 
 const VANILLA_ARGS: &str = "-Xmx2G -XX:+UnlockExperimentalVMOptions -XX:+UseG1GC -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M";
+const ICON: &str = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAAGFBMVEUAAAB7GWmyF1rqR1a3msT/nJfp0+r///+virVnAAAAAXRSTlMAQObYZgAAAI9JREFUKM990cENA0EIQ9FpgRbcglugBVr4Lbj9HCaRNtnZcHwSYMFaa63Vvb7rB5JS8QeiAqCfIA0C6yO/0CFJDQkcgWqSsTIlTpCSSJJoCAdAHqtqY/oOncgz492pIyTInrEK7kDtrci21AeQx+9olxwXWP0e2GRf4AaLRPYOV32CbSVb1TzAtsDloR94AcQTfFFwBa/NAAAAAElFTkSuQmCC";
 
 #[derive(Serialize, Deserialize)]
 struct LauncherProfiles {
@@ -34,34 +35,34 @@ pub fn write_profile(mc: &String, velvet: &String, x: &File) -> String {
 
     // This copies the "Latest Release" java arguments and version.
     let mut args = VANILLA_ARGS;
-    let mut dir = "";
+    let mut dir = &Value::Null;
     for x in json.profiles.values() {
         if x["type"] == "latest-release" {
             println!("Copying Java directory and arguments from: {} profile.", "Latest Release".purple().italic());
             if !x["javaArgs"].is_null() {
                 args = x["javaArgs"].as_str().unwrap();
             }
-            if !x["javaDir"].is_null() {
-                dir = x["javaDir"].as_str().unwrap();
-            }
+            dir = &x["javaDir"];
             break
         }
     }
     let time = Utc::now();
 
-    let new_profile = json!({
-        "created": &time,
-        "icon": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgBAMAAACBVGfHAAAAGFBMVEUAAAB7GWmyF1rqR1a3msT/nJfp0+r///+virVnAAAAAXRSTlMAQObYZgAAAI9JREFUKM990cENA0EIQ9FpgRbcglugBVr4Lbj9HCaRNtnZcHwSYMFaa63Vvb7rB5JS8QeiAqCfIA0C6yO/0CFJDQkcgWqSsTIlTpCSSJJoCAdAHqtqY/oOncgz492pIyTInrEK7kDtrci21AeQx+9olxwXWP0e2GRf4AaLRPYOV32CbSVb1TzAtsDloR94AcQTfFFwBa/NAAAAAElFTkSuQmCC",
-        "javaArgs": format!("-Dloader.modsDir=.velvet/mods/{0} -Dloader.configDir=.velvet/config/{0} {1}", &mc, &args),
-        "lastUsed": &time,
-        "lastVersionId": format!("quilt-loader-{}-{}", &velvet, &mc),
-        "name": format!("Velvet {}", &mc),
-        "type": "custom",
-    });
-    if !dir.is_empty() {
-        json.profiles.insert(format!("velvet-quilt-loader-{}", &mc), json!({"javaDir": &dir}));
+
+    let mut new_profile = HashMap::from([
+        ("created", json!(&time)),
+        ("icon", json!(ICON)),
+        ("javaArgs", json!(format!("-Dloader.modsDir=.velvet/mods/{0} -Dloader.configDir=.velvet/config/{0} {1}", &mc, &args))),
+        ("lastUsed", json!(&time)),
+        ("lastVersionId", json!(format!("quilt-loader-{}-{}", &velvet, &mc))),
+        ("name", json!(format!("Velvet {}", &mc))),
+        ("type", json!("custom")),
+        ]);
+
+    if let Value::String(entry) = dir {
+        new_profile.insert("javaDir", json!(&entry));
     }
-    json.profiles.insert(format!("velvet-quilt-loader-{}", &mc), new_profile);
+    json.profiles.insert(format!("velvet-quilt-loader-{}", &mc), json!(new_profile));
     serde_json::to_string_pretty(&json).unwrap()
 }
 
