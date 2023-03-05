80183ce8bac92af853f21054993c17de3a80e150
derspyy <65043391+derspyy@users.noreply.github.com>
2023-03-05 17:56:26 -0300

    Multi-threading :)

--- src/get_mods.rs
+++ src/get_mods.rs
@@ -1,8 +1,9 @@
-use std::{fs::File, io::Write, path::PathBuf};
+use std::{fs::File, io::Write, path::PathBuf, thread};
 
 use anyhow::{anyhow, Result};
 use serde_json::Value;
 use percent_encoding::percent_decode_str;
+
 
 const VANILLA: [&str; 8] = [
     "AANobbMI", // sodium
@@ -69,31 +70,49 @@ pub fn run(
     }
     mods.sort();
     mods.dedup();
+    let mut thread_vec = Vec::new();
     for x in mods {
-        let modrinth_url = format!("{}/{}/version?loaders=[\"fabric\", \"quilt\"]?game_versions=[{:?}]", MODRINTH_SERVER, x, mc_version);
-        let response: Value = agent
+        let agent = agent.clone();
+        let mc_version = mc_version.clone();
+        let base_path = base_path.clone();
+        thread_vec.push(thread::spawn(move || {
+            download_mod(x, agent, mc_version, base_path)
+        }));
+    }
+
+    for thread in thread_vec {
+        thread.join().unwrap()?
+    }
+
+    Ok(())
+}
+
+fn download_mod(x: &str, agent: ureq::Agent, mc_version: String, base_path: PathBuf) -> Result<(), anyhow::Error> {
+    println!("Installing {}", x);
+    let modrinth_url = format!("{}/{}/version?loaders=[\"fabric\", \"quilt\"]?game_versions=[{:?}]", MODRINTH_SERVER, x, mc_version);
+    let response: Value = agent
             .get(&modrinth_url)
             .call()?
             .into_json()?;
-        let versions = response[0]["files"].as_array()
+    let versions = response[0]["files"].as_array()
             .ok_or_else(|| anyhow!("Couldn't parse versions!"))?;
 
-        if !versions.is_empty() {
-            let url = versions[0]["url"].as_str()
+    if !versions.is_empty() {
+        let url = versions[0]["url"].as_str()
                 .ok_or_else(|| anyhow!("Couldn't parse versions!"))?;
-            let mut file_name = url
+        let mut file_name = url
                 .split('/')
                 .last()
                 .ok_or_else(|| anyhow!("Couldn't parse file name!"))?
                 .to_string();
-            file_name = percent_decode_str(&file_name).decode_utf8()?.into_owned();
-            let path = base_path.join(file_name).with_extension("jar");
-            let download = agent.get(url).call()?;
-            let mut bytes = Vec::new();
-            download.into_reader().read_to_end(&mut bytes)?;
-            let mut mod_file = File::create(path)?;
-            mod_file.write_all(&bytes)?;
-        }
+        file_name = percent_decode_str(&file_name).decode_utf8()?.into_owned();
+        let path = base_path.join(file_name).with_extension("jar");
+        let download = agent.get(url).call()?;
+        let mut bytes = Vec::new();
+        download.into_reader().read_to_end(&mut bytes)?;
+        let mut mod_file = File::create(path)?;
+        mod_file.write_all(&bytes)?;
     }
+    println!("Returning {}", x);
     Ok(())
 }
